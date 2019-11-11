de10742887d2b160f7d42ca9fe0724876c590ea2
alexpilk <alex.pilkevych@gmail.com>
2019-11-11 10:28:12 +0100

    Working bluetooth pairing

--- attiny/src/papryczka/papryczka.ino
+++ attiny/src/papryczka/papryczka.ino
@@ -6,78 +6,57 @@
 
 SoftwareSerial blueToothSerial(RxD, TxD);
 
-int led = 4;
+const int led = 4;
 const int buttonPin = 3;
 
 void setup()
 {
-  pinMode(RxD, INPUT);
-  pinMode(TxD, OUTPUT);
+  setPinModes();
   setupBlueToothConnection();
-
-  pinMode(led, OUTPUT);
-  pinMode(buttonPin, INPUT);
   digitalWrite(led, HIGH);
-
 }
 
-String cache = "";
-void loop(){
+void setPinModes() {
+  pinMode(RxD, INPUT);
+  pinMode(TxD, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
   int count = 0;
-//  char recvChar;
   while (1) {
-//    blueToothSerial.print("AT\n\r" + cache);
     delay(100);
-//    delay(3000);
-    //check if there's any data sent from the remote bluetooth shield
     if (blueToothSerial.available()) {
       String recvChar = blueToothSerial.readString();
-      blueToothSerial.print(recvChar+"o");
-//      recvChar = blueToothSerial.read();
-//      blueToothSerial.print(recvChar);
+      blueToothSerial.flush();
+      blueToothSerial.print(recvChar + "o");
       count++ ;
       if (count % 2 == 0)
         digitalWrite(led, HIGH);
-
       else
         digitalWrite(led, LOW);
     }
-//    delay(1000);
-    int buttonState = digitalRead(buttonPin);
-    if(buttonState == HIGH) {
-        digitalWrite(led, LOW);
-      blueToothSerial.print("pressed!");
-      
-    }
-//      blueToothSerial.print("\r\n");
+    checkButtons();
   }
-  
 }
-void write(String command){
-    blueToothSerial.print("\r\nAT+" + command + "\r\n"); //set the bluetooth name as "HC-05"
-    blueToothSerial.readString();
-    delay(1000);
-  
+void write(String command) {
+  blueToothSerial.print("\r\nAT+" + command + "\r\n");
+  blueToothSerial.readString();
+  delay(500);
+
 }
 void setupBlueToothConnection()
 {
-  blueToothSerial.begin(9600); //Set BluetoothBee BaudRate to default baud rate 38400
-//    blueToothSerial.print("\r\n+STWMOD=0\r\n"); //set the bluetooth work in slave mode
-//    blueToothSerial.print("\r\n+NAMEdupiuhakius\r\n"); //set the bluetooth name as "HC-05"
-    write("NAMEpapryczka");
-    write("ROLE0");
-    write("IMME0");
-    write("RESET");
-    write("NOTI1");
-//    blueToothSerial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
-//    blueToothSerial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden hereAT+PIN008888
-//    blueToothSerial.print("\r\nAT+TYPE1\r\n"); // Auto-connection should be forbidden here
-//    blueToothSerial.print("\r\nAT+PASS008888\r\n"); // Auto-connection should be forbidden here
-blueToothSerial.print("\r\nAT+ADDR?\r\n");
-delay(200);
-    cache = blueToothSerial.readString();
+  blueToothSerial.begin(9600);
+  write("DEFAULT");
+  write("RESET");
+  write("NAMEpapryczka");
+  write("ROLE0");
+  write("IMME0");
+  write("RESET");
+  write("NOTI1");
   delay(2000); // This delay is required.
-//  blueToothSerial.print("\r\n+INQ=1\r\n"); //make the slave bluetooth inquirable
   blueToothSerial.print("bluetooth connected!\n");
 
   delay(2000); // This delay is required.
--- main.py
+++ main.py
@@ -1,5 +1,94 @@
-from code import wifi#, server
+from code import wifi, server
 
 
-wifi.start_wifi()
+# wifi.start_wifi()
 # server.run()
+
+from machine import UART, Pin
+import time
+
+
+class Receiver:
+    def __init__(self):
+        self.uart = UART(1)
+        self.uart.init(baudrate=9600, bits=8, parity=None, stop=1, tx=17, rx=16)
+        self.led = Pin(4, Pin.OUT)
+
+        self.EVENTS = {
+            'Disconnected': self.reconnect,
+            'presse': self.on_button
+        }
+
+    def connect(self):
+        self.factory_default()
+        self.reset()
+        self.set_imme()
+        self.set_master()
+        self.reset()
+        self.start()
+        self.inquire()
+        self.connect_first()
+        self.loop()
+
+    def on_button(self):
+        print('YAY')
+        self.led.on()
+        time.sleep(1)
+        self.led.off()
+
+    def reconnect(self):
+        while True:
+            data = self.connect_first()
+            if 'Connected' in data:
+                return self.loop()
+            time.sleep(1)
+
+    def read(self):
+        return self.uart.read()
+
+    def run_command(self, command):
+        self.uart.write(command+'\r\n')
+        time.sleep(1)
+        return self.wait()
+        # print(self.uart.read())
+
+    def reset(self):
+        return self.run_command('AT+RESET')
+
+    def factory_default(self):
+        return self.run_command('AT+DEFAULT')
+
+    def set_master(self):
+        return self.run_command('AT+ROLE1')
+
+    def set_imme(self):
+        return self.run_command('AT+IMME1')
+
+    def start(self):
+        return self.run_command('AT+START')
+
+    def inquire(self):
+        return self.run_command('AT+INQ')
+
+    def connect_first(self):
+        return self.run_command('AT+CONN1')
+
+    def wait(self):
+        data = None
+        while not data:
+            data = self.read()
+        print(data)
+        return data
+
+    def loop(self):
+        while True:
+            data = self.wait()
+            for expected, action in self.EVENTS.items():
+                if expected in str(data):
+                    action()
+
+
+receiver = Receiver()
+
+if __name__ == '__main__':
+    receiver.connect()
